ae43e4a1c05ca960bfcd8ac7ed55472b1b660bf5
Faizan Choudhary <faizan2000461@st.jmi.ac.in>
2024-10-16 23:36:03 +0530

    rename

--- src/Basics.scala
+++ src/Basics.scala
@@ -1,6 +1,6 @@
 package com.learning.scala
 
-object Test extends App {
+object Basics extends App {
   // This can also be executed independently (standalone) because it is an object and extends 'App'
   println("Hello world")
 
