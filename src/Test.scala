c17906ef88c200d8dd2b4b45f8fc691f5e1ecbca
Faizan Choudhary <faizan2000461@st.jmi.ac.in>
2024-10-16 19:59:37 +0530

    values in scala

--- src/Test.scala
+++ src/Test.scala
@@ -3,4 +3,13 @@ package com.learning.scala
 object Test extends App {
   // This can also be executed independently (standalone) because it is an object and extends 'App'
   println("Hello world")
+
+  // Values in scala are like 'const', ie, cannot be reassigned
+  val anInt: Int = 42
+
+  // not necessary to mention the type of the value, Scala compiler is smart enough to find out what datatype the RHS is
+  // ie, compiler automatically assigns the type
+  val aBool = false
+
+  println(anInt)
 }
