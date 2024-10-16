413ed54b18bc692796a04a68a14f8a23619834c7
Faizan Choudhary <faizan2000461@st.jmi.ac.in>
2024-10-16 23:46:08 +0530

    OOPs concepts intro

---
+++ src/OOPs.scala
@@ -0,0 +1,14 @@
+package com.learning.scala
+
+object OOPs extends App {
+  // Scala is an OO language
+
+  // class and instance (instantiation is done with new keyword)
+  // class definitions can be blank (without {})
+  class Animal {
+    val age: Int = 0
+    def eat(): Unit = println("I am eating.")
+  }
+  val anAnimal = new Animal
+  anAnimal.eat()
+}
